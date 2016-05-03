26d16836fa9306bf3b81923ffa5e9c594c22fe12
Joel Ahn <joelahn123@yahoo.com>
2016-05-03 01:24:55 -0500

    minor alterations on style of navbar component

--- client/containers/navHeader.jsx
+++ client/containers/navHeader.jsx
@@ -5,43 +5,23 @@ import {browserHistory} from 'react-router';
 // Component
 import {NavButtons} from '../components/navBarComps/navButtons.jsx';
 
-// const navHeader = ({sts, dsp}) => (
-// 	<div className="jumbotron">
-// 		<div id="navName">
-// 			<h1>Donut Store</h1>
-// 		</div>
-// 		<div id="navRight">
-// 				We&#39;re open <b>Everyday</b>!
-// 				<br/>
-// 				5:00 am - 12:00 pm
-// 				<br/>
-// 				Exeptions during <b>Holidays</b>
-// 		</div>
-// 		<NavButtons sts={sts} dsp={dsp}/>
-// 	</div>
-// );
-
-const navHeader = React.createClass({
-	render(){
-		return (
-			<div className="jumbotron">
-				<div id="navName">
-					<h1>Donut Store</h1>
-				</div>
-				<div id="navRight">
-						We&#39;re open <b>Everyday</b>!
-						<br/>
-						5:00 am - 12:00 pm
-						<br/>
-						Exeptions during <b>Holidays</b>
-				</div>
-				<NavButtons sts={this.props.sts} dsp={this.props.dsp}/>
-				{console.log("props", this.props)}
-				{this.props.children}
-			</div>
-		);
-	}
-});
+const navHeader = ({sts, dsp, children}) => (
+	<div className="jumbotron">
+		<div id="navName">
+			<h1>Donut Store</h1>
+		</div>
+		<div id="navRight">
+				We&#39;re open <b>Everyday</b>!
+				<br/>
+				5:00 am - 12:00 pm
+				<br/>
+				Exeptions during <b>Holidays</b>
+		</div>
+		<NavButtons sts={sts} dsp={dsp}/>
+		{/* children loads the specified nested route component (see app.jsx for all routes) */}
+		{children}
+	</div>
+);
 
 const mapStateToProps = (state) => {
 	console.info("Container State: ", state);
