268b25de70e83122794375c25a2b04d099a8ddce
Joel Ahn <joelahn123@yahoo.com>
2016-05-06 15:24:47 -0500

    attempting migration/seeding in production

--- server/db.js
+++ server/db.js
@@ -4,24 +4,27 @@ const knex = require('knex')(config[env]);
 
 module.exports = knex;
 
-if(env === 'development'){
+if(env === 'development') {
 	knex.migrate.latest(config[env])
-		.then(function(){
+		.then(function() {
 			return knex.seed.run();
 		})
-		.then(function(){
+		.then(function() {
 			console.log("seeded!");
 		})
-		.catch(function(err){// unsure if this argument is actually an error, console logging to find out
+		.catch(function(err) {// unsure if this argument is actually an error, console logging to find out
 			console.log("doh! frickin seeds!", err);
 		})
 }
 else{
 	knex.migrate.latest(config[env])
-		.then(function(){
-			console.log("Production migration successful!")
+		.then(function() {
+			return knex.seed.run();
 		})
-		.catch(function(err){// unsure if this argument is actually an error, console logging to find out
-			console.log("uh oh production migration error!", err);
+		.then(function() {
+			console.log("Production migration/seed successful!");
+		})
+		.catch(function(err) {// unsure if this argument is actually an error, console logging to find out
+			console.log("uh oh production migration/seed error!", err);
 		})
 }
