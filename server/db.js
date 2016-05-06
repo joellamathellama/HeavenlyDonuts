c60ddc33c55711ede527b333707f38910c9109b7
Joel Ahn <joelahn123@yahoo.com>
2016-05-06 15:00:57 -0500

    migration/seed serpation for dev and prod env

--- server/db.js
+++ server/db.js
@@ -4,13 +4,24 @@ const knex = require('knex')(config[env]);
 
 module.exports = knex;
 
-// knex.migrate.latest(config[env])
-// 	.then(function(){
-// 		return knex.seed.run();
-// 	})
-// 	.then(function(){
-// 		console.log("seeded!");
-// 	})
-// 	.catch(function(err){
-// 		console.log("doh! frickin seeds!", err);
-// 	})
+if(env === development){
+	knex.migrate.latest(config[env])
+		.then(function(){
+			return knex.seed.run();
+		})
+		.then(function(){
+			console.log("seeded!");
+		})
+		.catch(function(err){// unsure if this argument is actually an error, console logging to find out
+			console.log("doh! frickin seeds!", err);
+		})
+}
+else{
+	knex.migrate.latest(config[env])
+		.then(function(){
+			console.log("Production migration successful!")
+		})
+		.catch(function(err){// unsure if this argument is actually an error, console logging to find out
+			console.log("uh oh production migration error!", err);
+		})
+}
